c5e2fc58b52aa8795eba46bc4765e6149594c52e
AnonymousAKES <yuuy0538@gmail.com>
2020-06-11 09:56:37 +0000

    minor fix

--- models/twolayer_convnet.py
+++ models/twolayer_convnet.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 import numpy as np
 import math
 
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
 class ConvNet(nn.Module):
 
     def __init__(self, channels):
